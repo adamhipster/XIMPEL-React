771c87d79bcb175fba8551cca0418ad0e3c2c0c9
melvin roest <melvinroest@gmail.com>
2018-02-21 13:10:22 +0100

    HMR is possible. The accept code has not been written yet, it has to be started with webpack-dev-server --hot --inline

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('app')
+// );
 
-// enable this when you're programming a progressive web app
-// see https://github.com/facebookincubator/create-react-app/issues/2398
-// registerServiceWorker();
+// if(module.hot) {
+
+// }
+
+// // enable this when you're programming a progressive web app
+// // see https://github.com/facebookincubator/create-react-app/issues/2398
+// // registerServiceWorker();
+
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from '../common/App'
+render(<AppContainer>
+ <App />
+</AppContainer>, document.getElementById('root'))
+if (module.hot) {
+    module.hot.accept('../common/App', () => {
+        render(<AppContainer>
+          <App />
+        </AppContainer>, document.getElementById('root'))
+    })
+}
